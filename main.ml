13379c0437c996b734dd116c34f76b1cb918f006
bzy-debug <71200607+bzy-debug@users.noreply.github.com>
2022-10-24 00:09:13 +0800

    up

--- main.ml
+++ main.ml
@@ -1,3 +1,12 @@
+open Interp
 open Infer
-let _ = inferType
-let () = print_endline "Hello, World"
+
+let print_type = fun x -> x |> inferType |> print_endline
+
+let id = Letfun("f", "x", Var "x", Var "f")
+let tw = Letfun("tw", "g", Letfun("app", "x", Call(Var "g", Call(Var "g", Var "x")), Var "app"), Var "tw")
+let s x y z = (x z) (y z)
+let se = Letfun("s", "x", Letfun("a1", "y", Letfun("a2", "z", Call(Call(Var "x", Var "z"), Call(Var"y", Var "z")), Var "a2"), Var "a1"), Var"s")
+
+let exprs = [id; tw; se]
+let () = List.iter print_type exprs
